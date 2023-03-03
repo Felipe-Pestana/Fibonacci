0c2627d7a57f63eef9640eeb35f06db63f4ee6bf
Felipe Pestana <pestana@sitic.com.br>
2023-03-03 14:05:26 -0300

    Add project files.

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,12 @@
+﻿double ant = 1, prox = 0;
+int posicao;
+
+Console.Write("Informe a posicao desejada: ");
+posicao = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < posicao; i++)
+{
+    Console.Write(ant + " ");
+    ant = prox + ant;
+    prox = ant - prox;
+}
